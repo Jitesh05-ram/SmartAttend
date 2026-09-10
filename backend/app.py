250ac5bbe5db1882d8bb1a3e36337a44b7d66d36
Jitesh05-ram <jiteshram73183@gmail.com>
2026-09-10 13:55:23 +0530

    Prepare SmartAttend for Render deployment

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, app
 
 from backend.config import Config
 from backend import db, login_manager, mail, csrf
--- backend/config.py
+++ backend/config.py
@@ -51,7 +51,7 @@ class Config:
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = "Lax"
     SESSION_COOKIE_SECURE = (
-        os.getenv("SESSION_COOKIE_SECURE", "True").lower() == "true"
+        os.getenv("SESSION_COOKIE_SECURE", "False").lower() == "true"
     )
     REMEMBER_COOKIE_HTTPONLY = True
     REMEMBER_COOKIE_SAMESITE = "Lax"
@@ -112,7 +112,7 @@ class Config:
     REQUIRE_EMAIL_VERIFICATION = (
         os.getenv(
             "REQUIRE_EMAIL_VERIFICATION",
-            "True"
+            "False"
         ).lower() == "true"
     )
 
